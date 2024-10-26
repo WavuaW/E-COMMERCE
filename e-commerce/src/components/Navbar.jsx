3bcdb6f90ae3513b5075fbddc46cd0d69b180612
WavuaW <wavuamuroka@gmail.com>
2024-10-26 23:59:41 +0300

    fixing dependencies

--- e-commerce/src/components/Navbar.jsx
+++ e-commerce/src/components/Navbar.jsx
@@ -1,28 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search'
 
 const Container = styled.div`
     height: 60px;
-`
+`;
 
 const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
-`
+`;
 
-const Left = styled.div``
-const Center = styled.div``
-const Right= styled.div``
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Lugha = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  
+`;
+
+const Right= styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-            <Left>left</Left>
+            <Left>
+              <Lugha>EN</Lugha>
+              <SearchContainer>
+                input
+                <SearchIcon />
+              </SearchContainer>
+            </Left>
             <Center>center</Center>
             <Right>right</Right>
         </Wrapper>
-        
     </Container>
   )
 }
