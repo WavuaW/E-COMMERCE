df94339600108f5ef0020a3c09c7b60ddc690dd6
WavuaW <wavuamuroka@gmail.com>
2024-10-27 22:58:11 +0300

    Slider

---
+++ e-commerce/src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import { styled } from "styled-components"
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
+`;
+
+const Announcement = () => {
+  return (
+    <Container>
+        Deal Of A Lifetime! Up To 70% Off All Products!
+    </Container>
+  )
+}
+
+export default Announcement
