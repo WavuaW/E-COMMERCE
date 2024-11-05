5a351a6b37b34d76912d9bbd6d8356992b642a6f
WavuaW <wavuamuroka@gmail.com>
2024-11-05 09:42:34 +0300

    Adding the slider chandes

--- e-commerce/src/components/Slider.jsx
+++ e-commerce/src/components/Slider.jsx
@@ -1,12 +1,15 @@
 import { styled } from "styled-components"
 import ArrowLeftOutlinedIcon from "@mui/icons-material/ArrowLeftOutlined"
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined'
+import MyImage from '../images/Image1-prettypink.png'
+
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -29,6 +32,7 @@ const Arrow = styled.div`
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
 `;
 
 const Slide = styled.div`
@@ -36,6 +40,7 @@ const Slide = styled.div`
     height: 100vh;
     display: flex;  
     align-items: center;
+    background-color: #${props=>props.bg};
 `;
 const ImgContainer = styled.div`
     height: 100%;
@@ -48,12 +53,22 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-  
+    font-size: 70px;
 `;
 
-const Desc = styled.p``
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
 
-const Button = styled.button``
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
 
 const Image = styled.img`
     height: 80%;
@@ -66,13 +81,33 @@ const Slider = () => {
         <ArrowLeftOutlinedIcon />
       </Arrow>
       <Wrapper>
-        <Slide>
+        <Slide bg="f5fafd">
           <ImgContainer>
-            <Image src="https://images.unsplash.com/photo-1603189343302-e603f7add05a?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            <Image src={MyImage}/>
           </ImgContainer>
           <InfoContainer>
-            <Title>SUMMER SALE</Title>
-            <Desc>DON'T COMPROMISE ON PRICE! GET FLAT 30% OFF ALL NEW ARRIVALS!!</Desc>
+            <Title>SUMMER SALE!</Title>
+            <Desc>DON'T COMPROMISE ON PRICE! GET FLAT 30% OFF ALL NEW ARRIVALS!</Desc>
+            <Button>SHOP NOW!</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+          <ImgContainer>
+            <Image src={MyImage}/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>WINTER SALE!</Title>
+            <Desc>DON'T COMPROMISE ON PRICE! GET FLAT 30% OFF ALL NEW ARRIVALS!</Desc>
+            <Button>SHOP NOW!</Button>
+          </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f">
+          <ImgContainer>
+            <Image src={MyImage}/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE!</Title>
+            <Desc>DON'T COMPROMISE ON PRICE! GET FLAT 30% OFF ALL NEW ARRIVALS!</Desc>
             <Button>SHOP NOW!</Button>
           </InfoContainer>
         </Slide>
