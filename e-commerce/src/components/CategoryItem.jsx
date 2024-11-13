1102ac15aa5c8f1aee46e09fcadd2fb0ada6b93b
WavuaW <wavuamuroka@gmail.com>
2024-11-13 21:05:48 +0300

    Categories

--- e-commerce/src/components/CategoryItem.jsx
+++ e-commerce/src/components/CategoryItem.jsx
@@ -2,12 +2,25 @@ import styled from "styled-components";
 import { categories } from "../data";
 
 const Container = styled.div`
+    flex: 1;
+`;
+const Image = styled.img`
+`;
+const Title = styled.h1`
+`;
+const Info = styled.div`
+`;
+const Button = styled.button`
 `;
 
 const CategoryItem = ({item}) => {
   return (
     <Container>
-       Hello
+       <Image src={item.img}/>
+       <Info>
+            <Title>{item.title}</Title>
+            <Button>SHOP NOW</Button>
+       </Info>
     </Container>
   )
 }
