132597e216c1def5bf8f19b87a86c4864e93c145
WavuaW <wavuamuroka@gmail.com>
2025-02-07 18:58:22 +0300

    finishing the category item

--- e-commerce/src/components/CategoryItem.jsx
+++ e-commerce/src/components/CategoryItem.jsx
@@ -7,12 +7,17 @@ const Container = styled.div`
     height: 70vh;
     position: relative;
 `;
+
 const Image = styled.img`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    background-color: black;
+
 `;
 const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
 `;
 const Info = styled.div`
     position: absolute;
@@ -21,10 +26,17 @@ const Info = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 `;
 const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color: gray;
+    cursor: pointer;
+    font-weight: 600;
 `;
 
 const CategoryItem = ({item}) => {
