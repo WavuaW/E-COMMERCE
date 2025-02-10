75f9445d028c113cc83886f6da12d54ad04fdb56
WavuaW <wavuamuroka@gmail.com>
2025-02-10 10:15:12 +0300

    products component

--- e-commerce/src/components/Product.jsx
+++ e-commerce/src/components/Product.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div``
+
 const Product = () => {
   return (
-    <div>
+    <Container>
       
-    </div>
+    </Container>
   )
 }
 
