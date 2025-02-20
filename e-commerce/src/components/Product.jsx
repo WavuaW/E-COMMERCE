8c33577200a93ca3c264184e7ac0c00d80e21b12
WavuaW <wavuamuroka@gmail.com>
2025-02-20 21:24:01 +0300

    styling the icons

--- e-commerce/src/components/Product.jsx
+++ e-commerce/src/components/Product.jsx
@@ -36,14 +36,29 @@ const Info = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  background-color: gray;
+  background-color: rgba(0,0,0,0.2);
   z-index: 3;
   display: flex;
   align-items: center;
   justify-content: center;
 
 `;
-const Icon = styled.div``;
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+
+  &:hover{
+    background-color: #e9f5f5;
+    transform: scale(1.1)
+  }
+`;
 
 const Product = ({item}) => {
   return (
